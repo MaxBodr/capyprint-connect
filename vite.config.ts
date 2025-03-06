a08408a298b65576dc688bdfceaaf5c5076ba799
Maxim Bodrov <macsimbodrov@gmail.com>
2025-03-06 11:51:11 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,25 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/capyprint-connect/", // Название репозитория
-  plugins: [react()],
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "index.html",
     },
   },
 }));
