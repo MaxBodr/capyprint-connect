0cf73e9daeee7b8b35250ce740f5f6697d76f9f9
Maxim Bodrov <macsimbodrov@gmail.com>
2025-03-06 08:05:09 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "capyprint-connect",
+  base: "/capyprint-connect/",
   server: {
     host: "::",
     port: 8080,
