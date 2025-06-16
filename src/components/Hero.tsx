c9bcdec66805dc0b51ec443dcad7522c074491a1
Maxim Bodrov <macsimbodrov@gmail.com>
2025-06-16 22:49:13 +0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -51,7 +51,7 @@ const Hero = () => {
         </motion.div>
 
         {/* Единый png-мокап */}
-        <div className="relative flex items-center justify-center">
+        <div className="relative flex items-center justify-center -mt-8 sm:-mt-12">
           <img
             src="/lovable-uploads/combined-mockup.png"
             alt="Мокап CapyPrint"
